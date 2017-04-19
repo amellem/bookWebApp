ed464d4e3390b76bd85a6c088cd702ba4bee3754
Jim Lombardo <james.g.lombardo@gmail.com>
2017-04-19 13:18:26 -0500

    Anthony's bookWebApp - fixed launch problems (by instructor)

---
+++ src/main/java/edu/wctc/asm/bookwebapp/controller/BookController.java
@@ -0,0 +1,172 @@
+package edu.wctc.asm.bookwebapp.controller;
+
+import edu.wctc.asm.bookwebapp.entity.Author;
+import edu.wctc.asm.bookwebapp.entity.Book;
+import edu.wctc.asm.bookwebapp.service.AuthorService;
+import edu.wctc.asm.bookwebapp.service.BookService;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author CloudAerius
+ */
+@WebServlet(name = "BookController", urlPatterns = {"/BookController"})
+public class BookController extends HttpServlet {
+
+    private AuthorService authorService;
+    private BookService bookService;
+
+    private String resultPage;
+    private String authorId;
+    private String bookId;
+
+    private void refreshList(BookService bs, HttpServletRequest request)
+            throws ClassNotFoundException, SQLException {
+        List<Book> b = bs.findAll();
+        request.setAttribute("books", b);
+    }
+
+    private void authorList(AuthorService as, HttpServletRequest request)
+            throws ClassNotFoundException, SQLException {
+        List<Author> a = as.findAll();
+        request.setAttribute("authors", a);
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String action = request.getParameter("action");
+        authorId = request.getParameter("selectedAuthor");
+        bookId = request.getParameter("selectedBook");
+
+        List<Author> authors = null;
+        
+        try {
+
+            switch (action) {
+
+                case "bookIndex":
+                    resultPage = "bookList.jsp";
+                    refreshList(bookService, request);
+                    break;
+
+                case "bookAdd":
+                    resultPage = "bookAdd.jsp";
+                    authorList(authorService, request);
+                    break;
+
+                case "bookToAdd":
+                    resultPage = "bookToAdd.jsp";
+                    if (authorId != null) {
+                        authors = authorService.findAll();
+                        for (Author a : authors) {
+                            if (Objects.equals(a.getAuthorId(), Integer.valueOf(authorId))) {
+                                request.setAttribute("authorName", a.getAuthorName());
+                                request.setAttribute("selectedAuthor", a.getAuthorId());
+                            }
+                        }
+                    }
+                    refreshList(bookService, request);
+                    break;
+
+                case "confirmAdd":
+                    resultPage = "bookList.jsp";
+                    bookService.addNew(authorId , request.getParameter("bookTitle"), request.getParameter("isbn"));
+                    refreshList(bookService, request);
+                    break;
+                    
+                case "bookUpdate":
+                    resultPage = "bookUpdate.jsp";
+                    refreshList(bookService, request);
+                    break;
+                    
+                case "bookToUpdate":
+                    resultPage = "confirmBookUpdate.jsp";
+                    if(bookId != null){
+                        List<Book> books = bookService.findAll();
+                        for (Book b : books){
+                            if (Objects.equals(b.getBookId(), Integer.valueOf(bookId))){
+                                request.setAttribute("bookId", b.getBookId());
+                                request.setAttribute("bookTitle", b.getTitle());
+                                request.setAttribute("isbn", b.getIsbn());
+                            }
+                        }
+                    }
+                    break;
+                    
+                case "updateBookConfirmed":
+                    resultPage = "bookList.jsp";
+                    bookService.update(request.getParameter("bookId"), request.getParameter("bookTitle"), request.getParameter("isbn"));
+                    refreshList(bookService, request);
+                    break;
+                    
+                case "bookDelete":
+                    resultPage = "bookDelete.jsp";
+                    refreshList(bookService, request);
+                    break;  
+                    
+                case "bookToDelete":
+                    resultPage = "bookList.jsp";
+                    bookService.remove(bookService.findById(request.getParameter("bookId")));
+                    refreshList(bookService, request);
+                    break;
+
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            request.setAttribute("errorMsg", e.getMessage());
+        }
+
+        RequestDispatcher view
+                = request.getRequestDispatcher(response.encodeRedirectURL(resultPage));
+
+        view.forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ src/main/java/edu/wctc/asm/bookwebapp/service/AuthorService.java
@@ -0,0 +1,102 @@
+package edu.wctc.asm.bookwebapp.service;
+
+import edu.wctc.asm.bookwebapp.entity.Author;
+import edu.wctc.asm.bookwebapp.repository.AuthorRepository;
+import edu.wctc.asm.bookwebapp.repository.BookRepository;
+import java.util.Date;
+import java.util.List;
+import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author CloudAerius
+ */
+@Service
+@Transactional(readOnly = true)
+public class AuthorService {
+
+    private transient final Logger LOG = LoggerFactory.getLogger(AuthorService.class);
+
+    @Inject
+    private AuthorRepository authorRepo;
+
+    @Inject
+    private BookRepository bookRepo;
+
+    public AuthorService() {
+    }
+
+    public List<Author> findAll() {
+        return authorRepo.findAll();
+    }
+    
+    public List<Author> findAllEagerly() {
+        List<Author> authors = authorRepo.findAll();
+        for(Author a : authors) {
+            a.getBookSet().size();
+        }
+        
+        return authors;
+    }
+    
+    /**
+     * This custom method is necessary because we are using Hibernate which
+     * does not load lazy relationships (in this case Books).
+     * @param id
+     * @return 
+     */
+    public Author findByIdAndFetchBooksEagerly(String id) {
+        Integer authorId = new Integer(id);
+        Author author = authorRepo.findOne(authorId);
+
+        // Eagerly fetches books within a transaction
+        author.getBookSet().size();
+        
+        return author;
+    }
+
+    public Author findById(String id) {
+        return authorRepo.findOne(new Integer(id));
+    }
+
+    /**
+     * Spring performs a transaction with readonly=false. This
+     * guarantees a rollback if something goes wrong.
+     * @param author 
+     */
+    @Transactional
+    public void remove(Author author) {
+        LOG.debug("Deleting author: " + author.getAuthorName());
+        authorRepo.delete(author);
+    }
+
+    /**
+     * Spring performs a transaction with readonly=false. This
+     * guarantees a rollback if something goes wrong.
+     * @param author 
+     * @return  
+     */
+    @Transactional
+    public Author edit(Author author) {
+        return authorRepo.saveAndFlush(author);
+    }
+    
+    @Transactional
+    public void addNew(String name) {
+        Author a = new Author();
+        a.setAuthorName(name);
+        Date createDate = new Date();
+        a.setDateAdded(createDate);
+        this.edit(a);
+    }
+    
+    public void update(String id, String name) {
+        Author a = this.findById(id);
+        a.setAuthorName(name);
+        this.edit(a);
+    }
+}
